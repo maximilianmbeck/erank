0b07e284b78b8e76dcfe191eb86b4090a8e98adc
Maximilian Beck <maximilianbeck@live.de>
2022-07-15 11:53:51 +0200

    add default normalizers

--- erank/data/data_preparation.py
+++ erank/data/data_preparation.py
@@ -1,4 +1,5 @@
 import logging
+from torch import normal
 import torchvision
 import torch.utils.data as data
 import torchvision.transforms as transforms
@@ -9,13 +10,19 @@ from hydra.utils import get_original_cwd
 LOGGER = logging.getLogger(__name__)
 
 FMNIST_NORMALIZER = {'mean': [0.2860], 'std': [0.3205]}
+CIFAR10_NORMALIZER = {}
 
 def prepare_fashion_mnist(dataset_kwargs: DictConfig) -> data.Dataset:
     LOGGER.info('Prepare Fashion-MNIST dataset.')
     data_dir = Path(get_original_cwd()) / dataset_kwargs.dataset_dir
-    normalizer = dataset_kwargs.normalizer
+    normalizer = dataset_kwargs.get('normalizer', None)
+    if normalizer is None:
+        LOGGER.info(f'Using default normalizer: {FMNIST_NORMALIZER}')
+        normalizer = FMNIST_NORMALIZER
+    else:
+        LOGGER.info('Using custom normalizer.')
     transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize(normalizer.mean, normalizer.std)])
+                                    transforms.Normalize(normalizer['mean'], normalizer['std'])])
     train_dataset = torchvision.datasets.FashionMNIST(root=data_dir, train=True, transform=transform, download=True)
     return train_dataset
 
