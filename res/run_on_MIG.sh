ff793296b9950d4126974434e3a389f808248eac
Maximilian Beck <maximilianbeck@live.de>
2022-07-20 16:43:40 +0200

    run exps 5.17,5.18

--- res/run_on_MIG.sh
+++ res/run_on_MIG.sh
@@ -12,8 +12,10 @@
 # 1 / 2
 CUDA_VISIBLE_DEVICES=MIG-GPU-c66f2b7d-c5d2-c0d1-e453-6f7f47a3b1d4/1/0 python run_sweep.py --config-name 5.7_cf10_cnn_erank_absmodel.yaml
 
-# 3 / 4
-CUDA_VISIBLE_DEVICES=MIG-GPU-915536bf-10cf-6704-559e-fe0de309c9bc/1/0 python run_sweep.py --config-name 5.7_cf10_cnn_erank_absmodel.yaml
+# 3
+CUDA_VISIBLE_DEVICES=MIG-GPU-915536bf-10cf-6704-559e-fe0de309c9bc/1/0 python run_sweep.py --config-name 5.17_cf10_cnn_erank_absmodel_datasetsize.yaml
+# 4
+CUDA_VISIBLE_DEVICES=MIG-GPU-915536bf-10cf-6704-559e-fe0de309c9bc/2/0 python run_sweep.py --config-name 5.18_cf10_fc_erank_absmodel_datasetsize.yaml
 
 ### BADGER
 # GPU 0: NVIDIA A100-PCIE-40GB (UUID: GPU-624f9f56-c3ca-cd9d-4faf-485756bca59f)
