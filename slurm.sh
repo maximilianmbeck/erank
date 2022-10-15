6578e0798c49d1291627616e4e952bd42c0bb189
Maximilian Beck <maximilianbeck@live.de>
2022-10-15 12:42:21 +0200

    setup exp up to 11.7

--- slurm.sh
+++ slurm.sh
@@ -21,4 +21,4 @@ which python
 NUM_CORES=32
 export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
 
-python run_sweep.py --config-name 11.3_mnist_conv4_rotatedtasks.yaml
+python run_sweep.py --config-name 11.7_mnist_lenet_rotatedtasks.yaml
