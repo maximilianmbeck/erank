75b7a8edbe74db3199f402de3cfbc907d409cfe6
Maximilian Beck <maximilianbeck@live.de>
2023-01-14 21:11:52 +0100

    add val_split transforms, rename kwargs

--- erank/data/datasetgenerator.py
+++ erank/data/datasetgenerator.py
@@ -34,9 +34,9 @@ class DatasetGenerator(DatasetGeneratorInterface):
         dataset_kwargs (Dict[str, Any]): Keyword args for the dataset.
         dataset_split (Dict[str, Any], optional): Keyword args for splitting the dataset. 
                                                   If not provided, the full dataset can be accessed via `val_split`. Defaults to {}.
-        train_dataset_transforms (Dict[str, Any], optional): Keyword args for adding transformation to the train split of the dataset.
+        train_split_transforms (Dict[str, Any], optional): Keyword args for adding transformation to the train split of the dataset.
                                                              If not provided, just try to normalize the dataset. Defaults to {}.
-        val_dataset_transforms (Dict[str, Any], optional): Keyword args for adding transformation to the val split of the dataset.
+        val_split_transforms (Dict[str, Any], optional): Keyword args for adding transformation to the val split of the dataset.
                                                            If not provided, just try to normalize the dataset. Defaults to {}.
     """
 
@@ -44,14 +44,14 @@ class DatasetGenerator(DatasetGeneratorInterface):
                  dataset: str,
                  dataset_kwargs: Dict[str, Any],
                  dataset_split: Dict[str, Any] = {},
-                 train_dataset_transforms: Dict[str, Any] = {},
-                 val_dataset_transforms: Dict[str, Any] = {}):
+                 train_split_transforms: Dict[str, Any] = {},
+                 val_split_transforms: Dict[str, Any] = {}):
         self.dataset = dataset
         self.dataset_class = get_dataset_class(dataset_name=dataset)
         self.dataset_kwargs = dataset_kwargs
         self.dataset_split = dataset_split
-        self.train_dataset_tansforms = train_dataset_transforms
-        self.val_dataset_transforms = val_dataset_transforms
+        self.train_dataset_tansforms = train_split_transforms
+        self.val_dataset_transforms = val_split_transforms
         self._train_split = None
         self._val_split = None
         self._dataset_generated = False
